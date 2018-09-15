577a4682bd78919467ce1a05adb351cb9a180a04
Stephen Longfield <stephen.longfield@gmail.com>
2018-09-15 11:07:03 -0700

    Header section decoded.

--- src/cpu/header.rs
+++ src/cpu/header.rs
@@ -1,4 +1,5 @@
 use std::fmt;
+use std::str;
 
 ///! Constants associated with the ROM header. Each of these is a range of bytes in the header.
 const NINTENDO: (usize, usize) = (0x0104, 0x0133);
@@ -15,6 +16,7 @@ const LICENSEE: (usize, usize) = (0x014B, 0x014B);
 const ROM_VERSION: (usize, usize) = (0x014C, 0x014C);
 const HEADER_CHECKSUM: (usize, usize) = (0x014D, 0x014D);
 const GLOBAL_CHECKSUM: (usize, usize) = (0x014E, 0x014E);
+const BIT_MASKS: [u8; 8] = [1 << 7, 1 << 6, 1 << 5, 1 << 4, 1 << 3, 1 << 2, 1 << 1, 1];
 
 pub struct Header {
     nintendo: Vec<u8>,
@@ -24,41 +26,94 @@ pub struct Header {
     licensee: String,
     sgb: bool,
     cartrigte_type: String,
-    // TODO(slongfield): These might be more useful as u32. str for now for ease of Display.
-    rom_size: String,
-    ram_size: String,
+    rom_size: u8,
+    ram_size: u8,
     destination_code: bool,
     rom_version: u8,
     header_checksum: u8,
-    global_checksum: u16,
+    global_checksum: u8,
 }
 
 impl Header {
     pub fn new(bytes: &Vec<u8>) -> Header {
         Header {
             nintendo: bytes[NINTENDO.0..(NINTENDO.1 + 1)].to_vec(),
-            title: "".to_string(),
-            manufacturer: 0,
-            gcb: false,
-            licensee: "".to_string(),
-            sgb: false,
-            cartrigte_type: "".to_string(),
-            rom_size: "0".to_string(),
-            ram_size: "0".to_string(),
-            destination_code: false,
-            rom_version: 0,
-            header_checksum: 0,
-            global_checksum: 0,
+            title: str::from_utf8(&bytes[TITLE.0..(TITLE.1)])
+                .unwrap()
+                .to_string(),
+            manufacturer: bytes_to_u32(&bytes[MANUFACTURER.0..(MANUFACTURER.1 + 1)]),
+            gcb: bytes[GCB.0] != 0,
+            licensee: decode_license(&bytes),
+            sgb: bytes[SGB.0] != 0,
+            cartrigte_type: decode_cartrigte_type(bytes[CARTRIDGE_TYPE.0]),
+            rom_size: bytes[ROM_SIZE.0],
+            ram_size: bytes[RAM_SIZE.0],
+            destination_code: bytes[DESTINATION_CODE.0] == 0,
+            rom_version: bytes[ROM_VERSION.0],
+            // TODO(slongfield): Verify checksum validity.
+            header_checksum: bytes[HEADER_CHECKSUM.0],
+            global_checksum: bytes[GLOBAL_CHECKSUM.0],
         }
+    }
+}
+
+fn bytes_to_u32(bytes: &[u8]) -> u32 {
+    let mut outp: u32 = 0;
+    for byte in bytes {
+        outp = outp << 8;
+        outp |= *byte as u32;
+    }
+    outp
+}
+
+fn bytes_to_u16(bytes: &[u8]) -> u16 {
+    let mut outp: u16 = 0;
+    for byte in bytes {
+        outp = outp << 8;
+        outp |= *byte as u16;
+    }
+    outp
+}
+
+///! Decodes the licensee codes.
+/// TODO(slongfield): Transcribe the full list.
+fn decode_license(bytes: &Vec<u8>) -> String {
+    match bytes_to_u16(&bytes[LICENSEE.0..(LICENSEE.1 + 1)]) {
+        0x00 => "None".to_string(),
+        0x01 => "Nintendo".to_string(),
+        0x33 => decode_extended_license(&bytes),
+        code => format!("Other: 0x{:x}", code),
+    }
+}
+
+///! Decode extended license code.
+/// TODO(slongfield): The decode here seems to be missing some documentation, figure out the
+/// algorithm and document. This works for now, though.
+fn decode_extended_license(bytes: &Vec<u8>) -> String {
+    match bytes_to_u16(&bytes[NEW_LICENSEE.0..(NEW_LICENSEE.1 + 1)]) {
+        0x3031 => "Nintendo (new)".to_string(),
+        code => format!("Other Extended License: 0x{:x}", code),
+    }
+}
+
+///! Decodes the type of the cartrigte.
+/// TODO(slongfield): This probably makes more sense, and is more useful, as an enum.
+fn decode_cartrigte_type(byte: u8) -> String {
+    match byte {
+        0x00 => "ROM ONLY".to_string(),
+        0x01 => "MBC1".to_string(),
+        0x02 => "MBC1+RAM".to_string(),
+        0x13 => "MBC3+RAM+BATTERY".to_string(),
+        0x1B => "MBC5+RAM+BATTERY".to_string(),
+        code => format!("Unknown cartrigte type: 0x{:x}", code),
     }
 }
 
 ///! The Nintendo logo is stored in memory as tiles. Special-purpose renderer here, so it can run without any other dependencies.
 pub fn draw_nintendo(bytes: &Vec<u8>, f: &mut fmt::Formatter) -> fmt::Result {
     let mut bits: Vec<bool> = vec![];
-    let bit_masks = vec![1 << 7, 1 << 6, 1 << 5, 1 << 4, 1 << 3, 1 << 2, 1 << 1, 1];
-    for (index, byte) in bytes.iter().enumerate() {
-        for mask in bit_masks.iter() {
+    for byte in bytes.iter() {
+        for mask in BIT_MASKS.iter() {
             bits.push(byte & mask != 0);
         }
     }
@@ -100,7 +155,18 @@ pub fn draw_nintendo(bytes: &Vec<u8>, f: &mut fmt::Formatter) -> fmt::Result {
 
 impl fmt::Display for Header {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "Nintendo header:\n")?;
-        draw_nintendo(&self.nintendo, f)
+        draw_nintendo(&self.nintendo, f)?;
+        write!(f, "\nTitle: {}\n", self.title)?;
+        write!(f, "Manufacturer Code: 0x{:x}\n", self.manufacturer)?;
+        write!(f, "Gameboy Color Game: {}\n", self.gcb)?;
+        write!(f, "Licensee: {}\n", self.licensee)?;
+        write!(f, "Super Gameboy Support: {}\n", self.sgb)?;
+        write!(f, "Cartridge type: {}\n", self.cartrigte_type)?;
+        write!(f, "ROM size: 0x{:02x}\n", self.rom_size)?;
+        write!(f, "RAM size: 0x{:02x}\n", self.ram_size)?;
+        write!(f, "ROM version: 0x{:02x}\n", self.rom_version)?;
+        write!(f, "Japan-only?: {}\n", self.destination_code)?;
+        write!(f, "Header checksum: 0x{:02x}\n", self.header_checksum)?;
+        write!(f, "Global checksum: 0x{:02x}\n", self.global_checksum)
     }
 }
