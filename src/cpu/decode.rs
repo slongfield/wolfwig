d0c810b4c3a11a42ff8f1bb5b8ec9d4a26ccced1
Stephen Longfield <stephen.longfield@gmail.com>
2018-09-17 23:28:58 -0700

    Decode load instructions.

--- src/cpu/decode.rs
+++ src/cpu/decode.rs
@@ -17,10 +17,10 @@ pub enum Op {
     Load(ShortReg, Address),
     Store(Address, ShortReg),
     WideStore(Address, WideReg),
-    StoreAndIncrement(WideReg, ShortReg),
-    StoreAndDecrement(WideReg, ShortReg),
-    LoadAndIncrement(ShortReg, WideReg),
-    LoadAndDecrement(ShortReg, WideReg),
+    StoreAndIncrement(Address, ShortReg),
+    StoreAndDecrement(Address, ShortReg),
+    LoadAndIncrement(ShortReg, Address),
+    LoadAndDecrement(ShortReg, Address),
     Unknown(u8),
 }
 
@@ -30,6 +30,15 @@ impl fmt::Display for Op {
             Op::Nop => write!(f, "NOP"),
             Op::AluOp(op) => write!(f, "{}", op),
             Op::Jump(dest) => write!(f, "JP {}", dest),
+            Op::Move(src, dest) => write!(f, "LD {:?} {:?}", src, dest),
+            Op::Set(dest, val) => write!(f, "LD {:?} 0x{:x}", dest, val),
+            Op::SetWide(dest, val) => write!(f, "LD {:?} 0x{:x}", dest, val),
+            Op::Load(dest, addr) => write!(f, "LD {:?} ({:?})", dest, addr),
+            Op::Store(addr, src) => write!(f, "LD ({:?}) {:?}", addr, src),
+            Op::StoreAndIncrement(addr, src) => write!(f, "LD ({:?}+) {:?}", addr, src),
+            Op::StoreAndDecrement(addr, src) => write!(f, "LD ({:?}-) {:?}", addr, src),
+            Op::LoadAndIncrement(dest, addr) => write!(f, "LD {:?} ({:?}+)", dest, addr),
+            Op::LoadAndDecrement(dest, addr) => write!(f, "LD {:?} ({:?}-)", dest, addr),
             Op::Unknown(code) => write!(f, "Don't know how to display: 0x{:x}", code),
             _ => write!(f, "Missed case!"),
         }
@@ -69,7 +78,7 @@ pub enum AluOp {
 impl fmt::Display for AluOp {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            AluOp::Add(Data) => write!(f, "ADD A,{:?}", Data),
+            AluOp::Add(Data) => write!(f, "ADD A {:?}", Data),
             AluOp::AddWithCarry(Data) => write!(f, "ADC {:?}", Data),
             AluOp::And(Data) => write!(f, "AND {:?}", Data),
             AluOp::Compare(Data) => write!(f, "CMP {:?}", Data),
@@ -138,17 +147,6 @@ pub fn decode(rom: &Vec<u8>, pc: usize) -> (Op, usize) {
     }
     match rom[pc] {
         0x00 => (Op::Nop, 1),
-        0x01 => {
-            let val = cpu::bytes_to_u16(&rom[(pc + 1)..(pc + 3)]);
-            (Op::SetWide(BC, val), 3)
-        }
-        0x02 => (Op::Store(Address::Register16(BC), A), 1),
-        0x06 => (Op::Set(B, rom[pc + 1]), 2),
-        0x08 => {
-            let addr = cpu::bytes_to_u16(&rom[(pc + 1)..(pc + 3)]);
-            (Op::WideStore(Address::Immediate16(addr), SP), 3)
-        }
-        0x0A => (Op::Load(A, Address::Register16(BC)), 1),
         0xC3 => {
             let dest = cpu::bytes_to_u16(&rom[(pc + 1)..(pc + 3)]);
             (Op::Jump(Data::Immediate16(dest)), 3)
@@ -287,5 +285,125 @@ fn decode_alu(rom: &Vec<u8>, pc: usize) -> Option<(Op, usize)> {
 
 ///! Decode move, load, and store operations.
 fn decode_load(rom: &Vec<u8>, pc: usize) -> Option<(Op, usize)> {
-    None
+    let inst = match rom[pc] {
+        0x01 => (
+            Op::SetWide(BC, cpu::bytes_to_u16(&rom[(pc + 1)..(pc + 3)])),
+            3,
+        ),
+        0x11 => (
+            Op::SetWide(BC, cpu::bytes_to_u16(&rom[(pc + 1)..(pc + 3)])),
+            3,
+        ),
+        0x11 => (
+            Op::SetWide(BC, cpu::bytes_to_u16(&rom[(pc + 1)..(pc + 3)])),
+            3,
+        ),
+        0x11 => (
+            Op::SetWide(BC, cpu::bytes_to_u16(&rom[(pc + 1)..(pc + 3)])),
+            3,
+        ),
+
+        0x02 => (Op::Store(Address::Register16(BC), A), 1),
+        0x12 => (Op::Store(Address::Register16(DE), A), 1),
+        0x22 => (Op::StoreAndIncrement(Address::Register16(HL), A), 1),
+        0x32 => (Op::StoreAndDecrement(Address::Register16(HL), A), 1),
+
+        0x06 => (Op::Set(B, rom[pc + 1]), 2),
+        0x16 => (Op::Set(D, rom[pc + 1]), 2),
+        0x26 => (Op::Set(H, rom[pc + 1]), 2),
+        0x36 => (Op::SetWide(HL, rom[pc + 1] as u16), 2),
+
+        0x08 => {
+            let addr = cpu::bytes_to_u16(&rom[(pc + 1)..(pc + 3)]);
+            (Op::WideStore(Address::Immediate16(addr), SP), 3)
+        }
+
+        0x0E => (Op::Set(C, rom[pc + 1]), 2),
+        0x0E => (Op::Set(E, rom[pc + 1]), 2),
+        0x0E => (Op::Set(L, rom[pc + 1]), 2),
+        0x0E => (Op::Set(A, rom[pc + 1]), 2),
+
+        0x0A => (Op::Load(A, Address::Register16(BC)), 1),
+        0x1A => (Op::Load(A, Address::Register16(DE)), 1),
+        0x2A => (Op::LoadAndIncrement(A, Address::Register16(HL)), 1),
+        0x2A => (Op::LoadAndDecrement(A, Address::Register16(HL)), 1),
+
+        0x40 => (Op::Move(B, B), 1),
+        0x41 => (Op::Move(B, C), 1),
+        0x42 => (Op::Move(B, D), 1),
+        0x43 => (Op::Move(B, E), 1),
+        0x44 => (Op::Move(B, H), 1),
+        0x45 => (Op::Move(B, L), 1),
+        0x47 => (Op::Move(B, A), 1),
+
+        0x48 => (Op::Move(C, B), 1),
+        0x49 => (Op::Move(C, C), 1),
+        0x4A => (Op::Move(C, D), 1),
+        0x4B => (Op::Move(C, E), 1),
+        0x4C => (Op::Move(C, H), 1),
+        0x4D => (Op::Move(C, L), 1),
+        0x4F => (Op::Move(C, A), 1),
+
+        0x50 => (Op::Move(D, B), 1),
+        0x51 => (Op::Move(D, C), 1),
+        0x52 => (Op::Move(D, D), 1),
+        0x53 => (Op::Move(D, E), 1),
+        0x54 => (Op::Move(D, H), 1),
+        0x55 => (Op::Move(D, L), 1),
+        0x57 => (Op::Move(D, A), 1),
+
+        0x58 => (Op::Move(E, B), 1),
+        0x59 => (Op::Move(E, C), 1),
+        0x5A => (Op::Move(E, D), 1),
+        0x5B => (Op::Move(E, E), 1),
+        0x5C => (Op::Move(E, H), 1),
+        0x5D => (Op::Move(E, L), 1),
+        0x5F => (Op::Move(E, A), 1),
+
+        0x60 => (Op::Move(H, B), 1),
+        0x61 => (Op::Move(H, C), 1),
+        0x62 => (Op::Move(H, D), 1),
+        0x63 => (Op::Move(H, E), 1),
+        0x64 => (Op::Move(H, H), 1),
+        0x65 => (Op::Move(H, L), 1),
+        0x67 => (Op::Move(H, A), 1),
+
+        0x68 => (Op::Move(L, B), 1),
+        0x69 => (Op::Move(L, C), 1),
+        0x6A => (Op::Move(L, D), 1),
+        0x6B => (Op::Move(L, E), 1),
+        0x6C => (Op::Move(L, H), 1),
+        0x6D => (Op::Move(L, L), 1),
+        0x6F => (Op::Move(L, A), 1),
+
+        0x78 => (Op::Move(A, B), 1),
+        0x79 => (Op::Move(A, C), 1),
+        0x7A => (Op::Move(A, D), 1),
+        0x7B => (Op::Move(A, E), 1),
+        0x7C => (Op::Move(A, H), 1),
+        0x7D => (Op::Move(A, L), 1),
+        0x7F => (Op::Move(A, A), 1),
+
+        0x46 => (Op::Load(B, Address::Register16(HL)), 1),
+        0x4E => (Op::Load(C, Address::Register16(HL)), 1),
+        0x56 => (Op::Load(D, Address::Register16(HL)), 1),
+        0x5E => (Op::Load(E, Address::Register16(HL)), 1),
+        0x66 => (Op::Load(H, Address::Register16(HL)), 1),
+        0x6E => (Op::Load(L, Address::Register16(HL)), 1),
+        0x7E => (Op::Load(L, Address::Register16(HL)), 1),
+
+        0x70 => (Op::Store(Address::Register16(HL), B), 1),
+        0x71 => (Op::Store(Address::Register16(HL), C), 1),
+        0x72 => (Op::Store(Address::Register16(HL), D), 1),
+        0x73 => (Op::Store(Address::Register16(HL), E), 1),
+        0x74 => (Op::Store(Address::Register16(HL), H), 1),
+        0x75 => (Op::Store(Address::Register16(HL), L), 1),
+        0x77 => (Op::Store(Address::Register16(HL), A), 1),
+
+        code => (Op::Unknown(code), 0),
+    };
+    match inst {
+        (Op::Unknown(_), _) => None,
+        (op, size) => Some((op, size)),
+    }
 }
