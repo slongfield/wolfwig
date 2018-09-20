5e015dd4adf438dcec8e20fa76948dee3a41fc16
Stephen Longfield <stephen.longfield@gmail.com>
2018-09-19 23:52:47 -0700

    Added time information to ALU ops.

--- src/cpu/cpu.rs
+++ src/cpu/cpu.rs
@@ -10,7 +10,7 @@ impl Cpu {
     pub fn dump_instructions(&self, rom: &Vec<u8>, start_pc: usize, end_pc: usize) {
         let mut pc = start_pc;
         loop {
-            let (op, size) = cpu::decode::decode(rom, pc);
+            let (op, size, _) = cpu::decode::decode(rom, pc);
             println!("0x{:x}: {} ", pc, op);
             pc += size;
             if pc >= end_pc {
--- src/cpu/decode.rs
+++ src/cpu/decode.rs
@@ -135,156 +135,156 @@ impl fmt::Display for Data {
 }
 
 ///! Decode takes the ROM and current PC, and returns the Op a that PC, as well as the number of
-///! bytes in that op.
-pub fn decode(rom: &Vec<u8>, pc: usize) -> (Op, usize) {
+///! bytes in that op, and the number of cycles it runs for.
+pub fn decode(rom: &Vec<u8>, pc: usize) -> (Op, usize, usize) {
     match decode_alu(&rom, pc) {
-        Some((Op, usize)) => return (Op, usize),
+        Some((Op, size, time)) => return (Op, size, time),
         None => (),
     }
     match decode_load(&rom, pc) {
-        Some((Op, usize)) => return (Op, usize),
+        Some((Op, size, time)) => return (Op, size, time),
         None => (),
     }
     match rom[pc] {
-        0x00 => (Op::Nop, 1),
+        0x00 => (Op::Nop, 1, 1),
         0xC3 => {
             let dest = cpu::bytes_to_u16(&rom[(pc + 1)..(pc + 3)]);
-            (Op::Jump(Data::Immediate16(dest)), 3)
+            (Op::Jump(Data::Immediate16(dest)), 3, 3)
         }
-        code => (Op::Unknown(code), 1),
+        code => (Op::Unknown(code), 1, 0),
     }
 }
 
 ///! Decode ALU operations.
-fn decode_alu(rom: &Vec<u8>, pc: usize) -> Option<(Op, usize)> {
+fn decode_alu(rom: &Vec<u8>, pc: usize) -> Option<(Op, usize, usize)> {
     let inst = match rom[pc] {
-        0x03 => (AluOp::WideInc(BC), 1),
+        0x03 => (AluOp::WideInc(BC), 1, 1),
 
-        0x04 => (AluOp::Inc(B), 1),
-        0x14 => (AluOp::Inc(D), 1),
-        0x24 => (AluOp::Inc(H), 1),
-        0x34 => (AluOp::WideInc(HL), 1),
+        0x04 => (AluOp::Inc(B), 1, 1),
+        0x14 => (AluOp::Inc(D), 1, 1),
+        0x24 => (AluOp::Inc(H), 1, 1),
+        0x34 => (AluOp::WideInc(HL), 1, 1),
 
-        0x05 => (AluOp::Dec(B), 1),
-        0x15 => (AluOp::Dec(D), 1),
-        0x25 => (AluOp::Dec(H), 1),
-        0x35 => (AluOp::WideDec(HL), 1),
+        0x05 => (AluOp::Dec(B), 1, 1),
+        0x15 => (AluOp::Dec(D), 1, 1),
+        0x25 => (AluOp::Dec(H), 1, 1),
+        0x35 => (AluOp::WideDec(HL), 1, 1),
 
-        0x07 => (AluOp::RotateLeftIntoCarry, 1),
-        0x17 => (AluOp::RotateLeftThroughCarry, 1),
-        0x0F => (AluOp::RotateRightIntoCarry, 1),
-        0x1F => (AluOp::RotateRightThroughCarry, 1),
+        0x07 => (AluOp::RotateLeftIntoCarry, 1, 1),
+        0x17 => (AluOp::RotateLeftThroughCarry, 1, 1),
+        0x0F => (AluOp::RotateRightIntoCarry, 1, 1),
+        0x1F => (AluOp::RotateRightThroughCarry, 1, 1),
 
         // TODO(slongifeld) 0x27, 0x37, 0x2f, 0x3F
-        0x09 => (AluOp::WideAdd(HL, BC), 1),
-        0x19 => (AluOp::WideAdd(HL, DE), 1),
-        0x29 => (AluOp::WideAdd(HL, HL), 1),
-        0x39 => (AluOp::WideAdd(HL, SP), 1),
+        0x09 => (AluOp::WideAdd(HL, BC), 1, 2),
+        0x19 => (AluOp::WideAdd(HL, DE), 1, 2),
+        0x29 => (AluOp::WideAdd(HL, HL), 1, 2),
+        0x39 => (AluOp::WideAdd(HL, SP), 1, 2),
 
-        0x0B => (AluOp::WideDec(BC), 1),
-        0x1B => (AluOp::WideDec(DE), 1),
-        0x2B => (AluOp::WideDec(HL), 1),
-        0x3B => (AluOp::WideDec(SP), 1),
+        0x0B => (AluOp::WideDec(BC), 1, 2),
+        0x1B => (AluOp::WideDec(DE), 1, 2),
+        0x2B => (AluOp::WideDec(HL), 1, 2),
+        0x3B => (AluOp::WideDec(SP), 1, 2),
 
-        0x0C => (AluOp::Inc(C), 1),
-        0x1C => (AluOp::Inc(D), 1),
-        0x2C => (AluOp::Inc(L), 1),
-        0x3C => (AluOp::Inc(A), 1),
+        0x0C => (AluOp::Inc(C), 1, 1),
+        0x1C => (AluOp::Inc(D), 1, 1),
+        0x2C => (AluOp::Inc(L), 1, 1),
+        0x3C => (AluOp::Inc(A), 1, 1),
 
-        0x0D => (AluOp::Dec(D), 1),
-        0x1D => (AluOp::Dec(E), 1),
-        0x2D => (AluOp::Dec(L), 1),
-        0x3D => (AluOp::Dec(A), 1),
+        0x0D => (AluOp::Dec(D), 1, 1),
+        0x1D => (AluOp::Dec(E), 1, 1),
+        0x2D => (AluOp::Dec(L), 1, 1),
+        0x3D => (AluOp::Dec(A), 1, 1),
 
-        0x80 => (AluOp::Add(Data::Register8(B)), 1),
-        0x81 => (AluOp::Add(Data::Register8(C)), 1),
-        0x82 => (AluOp::Add(Data::Register8(D)), 1),
-        0x83 => (AluOp::Add(Data::Register8(E)), 1),
-        0x84 => (AluOp::Add(Data::Register8(H)), 1),
-        0x85 => (AluOp::Add(Data::Register8(L)), 1),
-        0x86 => (AluOp::Add(Data::Register16(HL)), 1),
-        0x87 => (AluOp::Add(Data::Register8(A)), 1),
-        0x88 => (AluOp::AddWithCarry(Data::Register8(B)), 1),
-        0x89 => (AluOp::AddWithCarry(Data::Register8(C)), 1),
-        0x8A => (AluOp::AddWithCarry(Data::Register8(D)), 1),
-        0x8B => (AluOp::AddWithCarry(Data::Register8(E)), 1),
-        0x8C => (AluOp::AddWithCarry(Data::Register8(H)), 1),
-        0x8D => (AluOp::AddWithCarry(Data::Register8(L)), 1),
-        0x8E => (AluOp::AddWithCarry(Data::Register16(HL)), 1),
-        0x8F => (AluOp::AddWithCarry(Data::Register8(A)), 1),
+        0x80 => (AluOp::Add(Data::Register8(B)), 1, 1),
+        0x81 => (AluOp::Add(Data::Register8(C)), 1, 1),
+        0x82 => (AluOp::Add(Data::Register8(D)), 1, 1),
+        0x83 => (AluOp::Add(Data::Register8(E)), 1, 1),
+        0x84 => (AluOp::Add(Data::Register8(H)), 1, 1),
+        0x85 => (AluOp::Add(Data::Register8(L)), 1, 1),
+        0x86 => (AluOp::Add(Data::Register16(HL)), 1, 1),
+        0x87 => (AluOp::Add(Data::Register8(A)), 1, 2),
+        0x88 => (AluOp::AddWithCarry(Data::Register8(B)), 1, 1),
+        0x89 => (AluOp::AddWithCarry(Data::Register8(C)), 1, 1),
+        0x8A => (AluOp::AddWithCarry(Data::Register8(D)), 1, 1),
+        0x8B => (AluOp::AddWithCarry(Data::Register8(E)), 1, 1),
+        0x8C => (AluOp::AddWithCarry(Data::Register8(H)), 1, 1),
+        0x8D => (AluOp::AddWithCarry(Data::Register8(L)), 1, 1),
+        0x8E => (AluOp::AddWithCarry(Data::Register16(HL)), 1, 2),
+        0x8F => (AluOp::AddWithCarry(Data::Register8(A)), 1, 1),
 
-        0x90 => (AluOp::Sub(Data::Register8(B)), 1),
-        0x91 => (AluOp::Sub(Data::Register8(C)), 1),
-        0x92 => (AluOp::Sub(Data::Register8(D)), 1),
-        0x93 => (AluOp::Sub(Data::Register8(E)), 1),
-        0x94 => (AluOp::Sub(Data::Register8(H)), 1),
-        0x95 => (AluOp::Sub(Data::Register8(L)), 1),
-        0x96 => (AluOp::Sub(Data::Register16(HL)), 1),
-        0x97 => (AluOp::Sub(Data::Register8(A)), 1),
-        0x98 => (AluOp::SubWithCarry(Data::Register8(B)), 1),
-        0x99 => (AluOp::SubWithCarry(Data::Register8(C)), 1),
-        0x9A => (AluOp::SubWithCarry(Data::Register8(D)), 1),
-        0x9B => (AluOp::SubWithCarry(Data::Register8(E)), 1),
-        0x9C => (AluOp::SubWithCarry(Data::Register8(H)), 1),
-        0x9D => (AluOp::SubWithCarry(Data::Register8(L)), 1),
-        0x9E => (AluOp::SubWithCarry(Data::Register16(HL)), 1),
-        0x9F => (AluOp::SubWithCarry(Data::Register8(A)), 1),
+        0x90 => (AluOp::Sub(Data::Register8(B)), 1, 1),
+        0x91 => (AluOp::Sub(Data::Register8(C)), 1, 1),
+        0x92 => (AluOp::Sub(Data::Register8(D)), 1, 1),
+        0x93 => (AluOp::Sub(Data::Register8(E)), 1, 1),
+        0x94 => (AluOp::Sub(Data::Register8(H)), 1, 1),
+        0x95 => (AluOp::Sub(Data::Register8(L)), 1, 1),
+        0x96 => (AluOp::Sub(Data::Register16(HL)), 1, 2),
+        0x97 => (AluOp::Sub(Data::Register8(A)), 1, 1),
+        0x98 => (AluOp::SubWithCarry(Data::Register8(B)), 1, 1),
+        0x99 => (AluOp::SubWithCarry(Data::Register8(C)), 1, 1),
+        0x9A => (AluOp::SubWithCarry(Data::Register8(D)), 1, 1),
+        0x9B => (AluOp::SubWithCarry(Data::Register8(E)), 1, 1),
+        0x9C => (AluOp::SubWithCarry(Data::Register8(H)), 1, 1),
+        0x9D => (AluOp::SubWithCarry(Data::Register8(L)), 1, 1),
+        0x9E => (AluOp::SubWithCarry(Data::Register16(HL)), 1, 2),
+        0x9F => (AluOp::SubWithCarry(Data::Register8(A)), 1, 1),
 
-        0xA0 => (AluOp::And(Data::Register8(B)), 1),
-        0xA1 => (AluOp::And(Data::Register8(C)), 1),
-        0xA2 => (AluOp::And(Data::Register8(D)), 1),
-        0xA3 => (AluOp::And(Data::Register8(E)), 1),
-        0xA4 => (AluOp::And(Data::Register8(H)), 1),
-        0xA5 => (AluOp::And(Data::Register8(L)), 1),
-        0xA6 => (AluOp::And(Data::Register16(HL)), 1),
-        0xA7 => (AluOp::And(Data::Register8(A)), 1),
+        0xA0 => (AluOp::And(Data::Register8(B)), 1, 1),
+        0xA1 => (AluOp::And(Data::Register8(C)), 1, 1),
+        0xA2 => (AluOp::And(Data::Register8(D)), 1, 1),
+        0xA3 => (AluOp::And(Data::Register8(E)), 1, 1),
+        0xA4 => (AluOp::And(Data::Register8(H)), 1, 1),
+        0xA5 => (AluOp::And(Data::Register8(L)), 1, 1),
+        0xA6 => (AluOp::And(Data::Register16(HL)), 1, 2),
+        0xA7 => (AluOp::And(Data::Register8(A)), 1, 1),
 
-        0xA8 => (AluOp::Xor(Data::Register8(B)), 1),
-        0xA9 => (AluOp::Xor(Data::Register8(C)), 1),
-        0xAA => (AluOp::Xor(Data::Register8(D)), 1),
-        0xAB => (AluOp::Xor(Data::Register8(E)), 1),
-        0xAC => (AluOp::Xor(Data::Register8(H)), 1),
-        0xAD => (AluOp::Xor(Data::Register8(L)), 1),
-        0xAE => (AluOp::Xor(Data::Register16(HL)), 1),
-        0xAF => (AluOp::Xor(Data::Register8(A)), 1),
+        0xA8 => (AluOp::Xor(Data::Register8(B)), 1, 1),
+        0xA9 => (AluOp::Xor(Data::Register8(C)), 1, 1),
+        0xAA => (AluOp::Xor(Data::Register8(D)), 1, 1),
+        0xAB => (AluOp::Xor(Data::Register8(E)), 1, 1),
+        0xAC => (AluOp::Xor(Data::Register8(H)), 1, 1),
+        0xAD => (AluOp::Xor(Data::Register8(L)), 1, 1),
+        0xAE => (AluOp::Xor(Data::Register16(HL)), 1, 2),
+        0xAF => (AluOp::Xor(Data::Register8(A)), 1, 1),
 
-        0xB0 => (AluOp::Or(Data::Register8(B)), 1),
-        0xB1 => (AluOp::Or(Data::Register8(C)), 1),
-        0xB2 => (AluOp::Or(Data::Register8(D)), 1),
-        0xB3 => (AluOp::Or(Data::Register8(E)), 1),
-        0xB4 => (AluOp::Or(Data::Register8(H)), 1),
-        0xB5 => (AluOp::Or(Data::Register8(L)), 1),
-        0xB6 => (AluOp::Or(Data::Register16(HL)), 1),
-        0xB7 => (AluOp::Or(Data::Register8(A)), 1),
+        0xB0 => (AluOp::Or(Data::Register8(B)), 1, 1),
+        0xB1 => (AluOp::Or(Data::Register8(C)), 1, 1),
+        0xB2 => (AluOp::Or(Data::Register8(D)), 1, 1),
+        0xB3 => (AluOp::Or(Data::Register8(E)), 1, 1),
+        0xB4 => (AluOp::Or(Data::Register8(H)), 1, 1),
+        0xB5 => (AluOp::Or(Data::Register8(L)), 1, 1),
+        0xB6 => (AluOp::Or(Data::Register16(HL)), 1, 2),
+        0xB7 => (AluOp::Or(Data::Register8(A)), 1, 1),
 
-        0xB8 => (AluOp::Compare(Data::Register8(B)), 1),
-        0xB9 => (AluOp::Compare(Data::Register8(C)), 1),
-        0xBA => (AluOp::Compare(Data::Register8(D)), 1),
-        0xBB => (AluOp::Compare(Data::Register8(E)), 1),
-        0xBC => (AluOp::Compare(Data::Register8(H)), 1),
-        0xBD => (AluOp::Compare(Data::Register8(L)), 1),
-        0xBE => (AluOp::Compare(Data::Register16(HL)), 1),
-        0xBF => (AluOp::Compare(Data::Register8(A)), 1),
+        0xB8 => (AluOp::Compare(Data::Register8(B)), 1, 1),
+        0xB9 => (AluOp::Compare(Data::Register8(C)), 1, 1),
+        0xBA => (AluOp::Compare(Data::Register8(D)), 1, 1),
+        0xBB => (AluOp::Compare(Data::Register8(E)), 1, 1),
+        0xBC => (AluOp::Compare(Data::Register8(H)), 1, 1),
+        0xBD => (AluOp::Compare(Data::Register8(L)), 1, 1),
+        0xBE => (AluOp::Compare(Data::Register16(HL)), 1, 2),
+        0xBF => (AluOp::Compare(Data::Register8(A)), 1, 1),
 
-        0xC6 => (AluOp::Add(Data::Immediate8(rom[(pc + 1)])), 1),
-        0xD6 => (AluOp::Sub(Data::Immediate8(rom[(pc + 1)])), 1),
-        0xE6 => (AluOp::And(Data::Immediate8(rom[(pc + 1)])), 1),
-        0xF6 => (AluOp::Or(Data::Immediate8(rom[(pc + 1)])), 1),
-        0xCE => (AluOp::AddWithCarry(Data::Immediate8(rom[(pc + 1)])), 1),
-        0xDE => (AluOp::SubWithCarry(Data::Immediate8(rom[(pc + 1)])), 1),
-        0xEE => (AluOp::Xor(Data::Immediate8(rom[(pc + 1)])), 1),
-        0xFE => (AluOp::Compare(Data::Immediate8(rom[(pc + 1)])), 1),
+        0xC6 => (AluOp::Add(Data::Immediate8(rom[(pc + 1)])), 1, 2),
+        0xD6 => (AluOp::Sub(Data::Immediate8(rom[(pc + 1)])), 1, 2),
+        0xE6 => (AluOp::And(Data::Immediate8(rom[(pc + 1)])), 1, 2),
+        0xF6 => (AluOp::Or(Data::Immediate8(rom[(pc + 1)])), 1, 2),
+        0xCE => (AluOp::AddWithCarry(Data::Immediate8(rom[(pc + 1)])), 1, 2),
+        0xDE => (AluOp::SubWithCarry(Data::Immediate8(rom[(pc + 1)])), 1, 2),
+        0xEE => (AluOp::Xor(Data::Immediate8(rom[(pc + 1)])), 1, 2),
+        0xFE => (AluOp::Compare(Data::Immediate8(rom[(pc + 1)])), 1, 2),
 
-        _ => (AluOp::Unknown, 0),
+        _ => (AluOp::Unknown, 0, 0),
     };
     match inst {
-        (AluOp::Unknown, _) => None,
-        (op, size) => Some((Op::AluOp(op), size)),
+        (AluOp::Unknown, _, _) => None,
+        (op, size, time) => Some((Op::AluOp(op), size, time)),
     }
 }
 
 ///! Decode move, load, and store operations.
-fn decode_load(rom: &Vec<u8>, pc: usize) -> Option<(Op, usize)> {
+fn decode_load(rom: &Vec<u8>, pc: usize) -> Option<(Op, usize, usize)> {
     let inst = match rom[pc] {
         0x01 => (
             Op::SetWide(BC, cpu::bytes_to_u16(&rom[(pc + 1)..(pc + 3)])),
@@ -404,6 +404,6 @@ fn decode_load(rom: &Vec<u8>, pc: usize) -> Option<(Op, usize)> {
     };
     match inst {
         (Op::Unknown(_), _) => None,
-        (op, size) => Some((op, size)),
+        (op, size) => Some((op, size, 1)),
     }
 }
